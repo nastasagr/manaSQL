2490e54f5edadcc1bc1b5f5989cc2497bfc571a1
nastasagr <alexanastagr@gmail.com>
2025-05-30 15:04:33 -0700

    tables list

--- auth.php
+++ auth.php
@@ -15,26 +15,26 @@
 
 <body x-data="dbLogin()" x-init="checkSession()">
 
+    <template x-if="errorMessage">
+        <!-- error message -->
+        <div class="error" x-show="errorMessage">
+            <div class="flex flex-row j-center i-center gap-2">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                            <path d="M4 6c0 1.657 3.582 3 8 3s8-1.343 8-3s-3.582-3-8-3s-8 1.343-8 3" />
+                            <path d="M4 6v6c0 1.657 3.582 3 8 3c1.118 0 2.182-.086 3.148-.241M20 12V6" />
+                            <path d="M4 12v6c0 1.657 3.582 3 8 3c1.064 0 2.079-.078 3.007-.22M19 16v3m0 3v.01" />
+                        </g>
+                    </svg>
+                </div>
 
-    <!-- error message -->
-    <div class="error" x-show="errorMessage">
-        <div class="flex flex-row j-center i-center gap-2">
-            <div>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                    <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-                        <path d="M4 6c0 1.657 3.582 3 8 3s8-1.343 8-3s-3.582-3-8-3s-8 1.343-8 3" />
-                        <path d="M4 6v6c0 1.657 3.582 3 8 3c1.118 0 2.182-.086 3.148-.241M20 12V6" />
-                        <path d="M4 12v6c0 1.657 3.582 3 8 3c1.064 0 2.079-.078 3.007-.22M19 16v3m0 3v.01" />
-                    </g>
-                </svg>
-            </div>
+                <div x-text="errorMessage">
 
-            <div x-text="errorMessage">
-
+                </div>
             </div>
         </div>
-    </div>
-
+    </template>
 
 
     <template x-if="!loggedIn">
@@ -76,11 +76,8 @@
     </template>
 
 
-
-
-
     <template x-if="loggedIn">
-        <div x-transition>
+        <div>
             <div>
                 <div class="logout" @click="logout()">
                     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 21 21">
@@ -89,13 +86,46 @@
                 </div>
             </div>
 
-            <div>
-                <div class="statebar">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512">
-                        <path fill="currentColor" fill-rule="evenodd" d="M426.666 298.667v64C426.666 409.795 350.256 448 256 448c-92.162 0-167.262-36.526-170.554-82.205l-.113-3.128v-64l.113 3.128C88.738 347.475 163.838 384 256 384c94.256 0 170.666-38.205 170.666-85.333M85.446 195.128c3.292 45.68 78.392 82.205 170.554 82.205c94.256 0 170.666-38.205 170.666-85.333v64l-.023 1.411c-1.507 46.478-77.33 83.922-170.643 83.922c-92.162 0-167.262-36.526-170.554-82.205L85.333 256v-64ZM256 64c92.162 0 167.262 36.526 170.554 82.205l.112 3.128l-.023 1.411c-1.507 46.478-77.33 83.923-170.643 83.923c-92.162 0-167.262-36.526-170.554-82.205l-.113-3.129C85.333 102.205 161.743 64 256 64" />
-                    </svg>
-                </div>
+
+
+            <div class="tables" x-show="tables.length">
+                <template x-for="table in tables" :key="table">
+                    <div class="table" :data-table="table"></div>
+                </template>
             </div>
+
+
+            <div class="statebar">
+                <div class="ibox">
+                    <div class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24">
+                            <path fill="currentColor" d="M11 13a1 1 0 1 0 1 1a1 1 0 0 0-1-1m-4 0a1 1 0 1 0 1 1a1 1 0 0 0-1-1m15-9a3 3 0 0 0-3-3H5a3 3 0 0 0-3 3v4a3 3 0 0 0 .78 2A3 3 0 0 0 2 12v4a3 3 0 0 0 3 3h6v2H3a1 1 0 0 0 0 2h18a1 1 0 0 0 0-2h-8v-2h6a3 3 0 0 0 3-3v-4a3 3 0 0 0-.78-2A3 3 0 0 0 22 8Zm-2 12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-4a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1Zm0-8a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1Zm-9-3a1 1 0 1 0 1 1a1 1 0 0 0-1-1M7 5a1 1 0 1 0 1 1a1 1 0 0 0-1-1" />
+                        </svg>
+                    </div>
+                    <div class="value" x-text="session.host"></div>
+                </div>
+
+                <div class="ibox">
+                    <div class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 16 16">
+                            <path fill="currentColor" fill-rule="evenodd" d="M8 8.5c1.776 0 3.515-.263 4.87-.888A5.6 5.6 0 0 0 14 6.931V8c0 2-2.686 3-6 3s-6-1-6-3V6.93c.35.275.736.501 1.13.682C4.485 8.237 6.224 8.5 8 8.5M14 4c0 2-2.686 3-6 3S2 6 2 4c0-.336.076-.643.217-.923C2.92 1.692 5.242 1 8 1c.828 0 1.618.063 2.335.188C12.49 1.563 14 2.5 14 4M8 15c3.314 0 6-1 6-3v-1.07c-.35.275-.736.501-1.13.683c-1.355.624-3.094.887-4.87.887s-3.515-.264-4.87-.887A5.7 5.7 0 0 1 2 10.93V12c0 2 2.686 3 6 3" clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                    <div class="value" x-text="session.database"></div>
+                </div>
+
+                <div class="ibox">
+                    <div class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24">
+                            <path fill="currentColor" d="M5 4h14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2m0 4v4h6V8zm8 0v4h6V8zm-8 6v4h6v-4zm8 0v4h6v-4z" />
+                        </svg>
+                    </div>
+                    <div class="value" x-text="tables.length"></div>
+                </div>
+
+
+            </div>
+
         </div>
     </template>
 
@@ -103,91 +133,17 @@
 
 
 
-    <script>
-        function dbLogin() {
-            return {
-                host: '',
-                user: '',
-                pass: '',
-                database: '',
-                loggedIn: false,
-                session: {},
-                errorMessage: '',
-                loading: true,
-                timeoutRef: null,
-
-                async connect() {
-                    try {
-                        const res = await fetch('actions.php?method=connect', {
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json'
-                            },
-                            body: JSON.stringify({
-                                host: this.host,
-                                user: this.user,
-                                pass: this.pass,
-                                database: this.database
-                            })
-                        });
-
-                        const data = await res.json();
-
-                        if (!res.ok || !data.success) {
-                            this.showMessage(data.message || 'Σφάλμα σύνδεσης.');
-                            return;
-                        }
-
-                        this.loggedIn = true;
-                        this.session = data.session;
-                    } catch (err) {
-                        this.showMessage('Αδυναμία σύνδεσης.');
-                    }
-                },
-
-                showMessage(msg) {
-                    this.errorMessage = msg;
 
 
-                    if (this.timeoutRef) {
-                        clearTimeout(this.timeoutRef);
-                    }
-
-                    this.timeoutRef = setTimeout(() => {
-                        this.errorMessage = '';
-                        this.timeoutRef = null;
-                    }, 3000);
-                },
 
 
-                async logout() {
-                    const res = await fetch('actions.php?method=logout', {
-                        method: 'POST'
-                    });
-                    const data = await res.json();
-                    if (data.success) {
-                        this.loggedIn = false;
-                        this.session = {};
-                    }
-                },
 
-                async checkSession() {
-                    try {
-                        const res = await fetch('actions.php?method=sessionCheck');
-                        const data = await res.json();
-                        if (data.loggedIn) {
-                            this.loggedIn = true;
-                            this.session = data.session;
-                        }
-                    } catch (e) {
 
-                    } finally {
-                        this.loading = false;
-                    }
-                }
-            };
-        }
-    </script>
+
+
+
+
+    <script src="manasql.js"></script>
 
 
 </body>
